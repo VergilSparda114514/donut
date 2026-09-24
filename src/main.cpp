05c0db3e02f11f32f21c6ec2988b917154b1c8c0
VergilSparda114514 <venalahennessy@gmail.com>
2026-09-25 00:00:17 +0800

    test scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
 #include <iostream>
+#include <random>
+#include <thread>
+
+static std::random_device rd{};
+static std::mt19937 rng{ rd() };
+
+static constexpr int fps = 30;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace ftxui;
+
+    Element document = hbox({
+        text("") | border | flex,
+    });
+    
+    Screen screen = Screen::Create(
+        Dimension::Fixed(80),
+        Dimension::Fixed(40)
+    );
+
+    std::uniform_int_distribution<> distx{ 1, 79 };
+    std::uniform_int_distribution<> disty{ 1, 39 };
+
+    int x = distx(rng), y = disty(rng);
+
+    while (true)
+    {
+        std::cout << screen.ResetPosition();
+        
+        Cell cell{};
+        screen.CellAt(x, y) = cell;
+
+        x = distx(rng);
+        y = disty(rng);
+
+        cell.character = "#";
+        screen.CellAt(x, y) = cell;
+
+        Render(screen, document);
+        screen.Print();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
+    }
 }
